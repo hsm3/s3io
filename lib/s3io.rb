2f9959f695108531721d0b1edd3eabfb16d8ea74
Arthur Pirogovski <arthur@flyingtealeaf.com>
2012-12-13 15:44:05 -0500

    docs added, swps removed

--- lib/s3io.rb
+++ lib/s3io.rb
@@ -3,7 +3,14 @@ require "s3io/wrapper"
 
 require "aws-sdk"
 
+# A top-level module that provides an S3 wrapper class.
 module S3io
+  # A shortcut for wrapping an S3 object
+  #
+  # @param [AWS::S3::S3Object] s3object an object to wrap
+  # @param [Hash] options options hash
+  # @option options [Integer] :line_buffer_size size of the buffer that is used for reading contents of S3 object when iterating over its lines
+  # @return [S3io::Wrapper] a wrapped S3 object
   def self.new(s3object, options = {})
     Wrapper.new(s3object, options)
   end
--- lib/s3io/wrapper.rb
+++ lib/s3io/wrapper.rb
@@ -1,13 +1,24 @@
 module S3io
+  # This class wraps an AWS S3 object in order to provide IO-like API.
+  # S3 objects wrapped this way can be used in methods that would otherwise expect an instance of File, StringIO etc.
   class Wrapper
 
+    # Default buffer size for line parser in bytes
     LINE_BUFFER_SIZE = 5 * 1024 * 1024 # MiB
 
     include Enumerable
 
+    # Current byte position in S3 object
     attr_accessor :pos
+
+    # Options that were passed during initialization
     attr_reader :options
 
+    # Wraps an AWS::S3::S3Object into IO-like object.
+    #
+    # @param [AWS::S3::S3Object] s3object an object to wrap
+    # @param [Hash] options options hash
+    # @option options [Integer] :line_buffer_size size of the buffer that is used for reading contents of S3 object when iterating over its lines
     def initialize(s3object, options = {})
       @s3object = s3object
       @options = {
@@ -17,6 +28,9 @@ module S3io
       @pos = 0
     end
 
+    # Reads data from S3 object.
+    #
+    # @param [Integer] bytes number of bytes to read
     def read(bytes = nil)
       content_length = @s3object.content_length
 
@@ -33,16 +47,21 @@ module S3io
       return data
     end
 
+    # Rewinds position to the very beginning of S3 object.
     def rewind
       @pos = 0
     end
 
+    # Iterates over S3 object lines.
+    #
+    # @param [String] separator line separator string
     def each(separator = $/)
       return enum_for(:each, separator) unless block_given?
 
       line = ''
       newline_pos = nil
 
+      # Either trying to parse the remainder or reading some more data
       while newline_pos || !(buffer = read(@options[:line_buffer_size])).empty?
         prev_newline_pos = newline_pos || 0
         newline_pos = buffer.index(separator, prev_newline_pos)
